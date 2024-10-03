1188fc08c667aa8c0e315186ed47ed0da093c954
donkim07 <donestomih@gmail.com>
2024-10-04 00:20:51 +0300

    pushed boostrap folder as well as first few web pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/login', function () {
+    return view('pages-login');
+});Route::get('/register', function () {
+    return view('pages-register');
+});Route::get('/profile', function () {
+    return view('users-profile');
 });
